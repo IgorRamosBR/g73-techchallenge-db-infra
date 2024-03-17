262f6775d47425fa4b3f2a4eaf9b9d891a252ab5
Igor Pestana <igor.pestana@greatcloudservices.com>
2024-03-17 15:07:39 -0300

    fixing instance class

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ resource "aws_db_instance" "g73_techchallenge_db" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  instance_class       = "db.t2.micro"
+  engine_version       = "16.1"
+  instance_class       = "db.t3.micro"
   name                 = "g73-techchallenge-db"
   username             = var.g73_techchallenge_db_username
   password             = var.g73_techchallenge_db_password
